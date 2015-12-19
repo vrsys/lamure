34c31f9429b32155a22a487a024cdba8a1a93218
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2015-12-19 22:02:52 +0100

    add header only unit testing library 'Catch' (https://github.com/philsquared/Catch) together with first entropy based reduction tests

---
+++ tests/test.cmake
@@ -0,0 +1,43 @@
+macro(InitTest TEST_NAME)
+
+  cmake_minimum_required(VERSION 2.8.7)
+  project(${TEST_NAME}_test)
+
+  file(GLOB HEADER_FILES ${PROJECT_SOURCE_DIR}/*.h)
+  file(GLOB SRC_FILES ${PROJECT_SOURCE_DIR}/*.cpp)
+
+  add_executable(${PROJECT_NAME} ${SRC_FILES} ${HEADER_FILES})
+  set_target_properties(${PROJECT_NAME} PROPERTIES OUTPUT_NAME ${TEST_NAME})
+
+  if(MSVC)
+    install(TARGETS ${PROJECT_NAME}
+      CONFIGURATIONS Release
+      RUNTIME DESTINATION test/Release
+      LIBRARY DESTINATION lib/Release
+      ARCHIVE DESTINATION lib/Release
+      )
+    install(TARGETS ${PROJECT_NAME}
+      CONFIGURATIONS Debug
+      RUNTIME DESTINATION test/Debug
+      LIBRARY DESTINATION lib/Debug
+      ARCHIVE DESTINATION lib/Debug
+      )
+  elseif(UNIX)
+    install(TARGETS ${PROJECT_NAME}
+      RUNTIME DESTINATION test
+      LIBRARY DESTINATION lib
+      ARCHIVE DESTINATION lib
+      )
+  endif(MSVC)
+endmacro()
+
+macro(MsvcPostBuild TARGET_NAME)
+  if(MSVC)
+    ADD_CUSTOM_COMMAND ( TARGET ${TARGET_NAME} POST_BUILD 
+      COMMAND robocopy \"${LIBRARY_OUTPUT_PATH}/$(Configuration)/\" \"${TEST_OUTPUT_PATH}/$(Configuration)/\" *.dll /R:0 /W:0 /NP 
+      & robocopy \"${GLOBAL_EXT_DIR}/\" \"${TEST_OUTPUT_PATH}/$(Configuration)/\" *.dll /R:0 /W:0 /NP 
+      & robocopy \"${GLOBAL_EXT_DIR}/\" \"${TEST_OUTPUT_PATH}/$(Configuration)/\" *.exe /R:0 /W:0 /NP \n if %ERRORLEVEL% LEQ 7 (exit /b 0) else (exit /b 1)
+      )
+  endif(MSVC)
+endmacro()
+
