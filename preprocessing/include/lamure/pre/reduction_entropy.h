aa9e2bb9a2c4c3bcd7c54b381b5c8587948a2c3d
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2015-12-20 14:49:21 +0100

    move entropy surfel struct & comparison operator out of class declaration, fix normal averaging

--- preprocessing/include/lamure/pre/reduction_entropy.h
+++ preprocessing/include/lamure/pre/reduction_entropy.h
@@ -20,17 +20,38 @@ namespace pre {
 
 class bvh;
 
-struct neighbour_distance_t{
-	surfel const* surfel_ptr;
-	real neighbour_distance;
-	surfel_id_t surfel_node_id;
-
-	neighbour_distance_t(surfel const* surf_ptr, real const distance, surfel_id_t const& s_n_id)
-	  :surfel_ptr(surf_ptr)
-	  ,neighbour_distance(distance)
-	  ,surfel_node_id(s_n_id)
-	  {}
+ struct entropy_surfel{
+	surfel* current_surfel;
+	uint32_t surfel_id;
+	uint32_t node_id;
+	std::vector<entropy_surfel*> neighbours;
+	bool validity;
+	uint16_t level;
+	float entropy;
 };
+
+struct min_entropy_order{
+	bool operator ()(entropy_surfel* const entropy_first, entropy_surfel* const entropy_second){
+
+		// true  : first goes to the front, second to the back
+		// false : first goes to the back, first to the front 
+
+		// if first is not valid, sort it to the front
+
+		bool is_rightmost = false;
+		if ( entropy_first->validity == false && entropy_second->validity == true) {
+
+			is_rightmost = true;
+		} else if (entropy_first->validity == true && entropy_second->validity == true){
+			if(entropy_first->entropy > entropy_second->entropy) {
+				is_rightmost = true;
+			}
+		}
+
+		return is_rightmost;
+	}
+};
+
 
 class reduction_entropy: public reduction_strategy
 {
@@ -42,39 +63,6 @@ public:
     surfel_mem_array      create_lod(real& reduction_error,
                                   const std::vector<surfel_mem_array*>& input,
                                   const uint32_t surfels_per_node) const override;
-
-	  struct entropy_surfel{
-		surfel* current_surfel;
-		uint32_t surfel_id;
-		uint32_t node_id;
-		std::vector<entropy_surfel*> neighbours;
-		bool validity;
-		uint16_t level;
-		float entropy;
-	};
-
-	struct min_entropy_order{
-		bool operator ()(entropy_surfel* const entropy_first, entropy_surfel* const entropy_second){
-
-			// true  : first goes to the front, second to the back
-			// false : first goes to the back, first to the front 
-
-			// if first is not valid, sort it to the front
-
-			bool is_rightmost = false;
-			if ( entropy_first->validity == false && entropy_second->validity == true) {
-
-				is_rightmost = true;
-			} else if (entropy_first->validity == true && entropy_second->validity == true){
-				if(entropy_first->entropy > entropy_second->entropy) {
-					is_rightmost = true;
-				}
-			}
-
-			return is_rightmost;
-		}
-	};
-
 private:
 
 
