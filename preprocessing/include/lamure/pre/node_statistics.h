3c53f208bf6435882260f116d0f335cf68f2654a
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2016-01-05 08:33:09 +0100

    add mean and standard deviation for all surfel attributes and color histogram in node statistics

--- preprocessing/include/lamure/pre/node_statistics.h
+++ preprocessing/include/lamure/pre/node_statistics.h
@@ -13,6 +13,10 @@
 namespace lamure {
 namespace pre {
 
+
+using histogram_t = 
+    std::array<std::vector<uint32_t>,3>;
+
 class node_statistics
 {
 public:
@@ -20,18 +24,46 @@ public:
 
 	void calculate_statistics(surfel_mem_array const& mem_array);
 
-	vec3r const mean() const {assert(is_dirty_ == false); 
-							  return mean_;}
-	real const sd() const {assert(is_dirty_ == false);
-						   return sd_;}
+	vec3r const mean_pos() const {assert(is_dirty_ == false); 
+                                      return mean_pos_;}
+	vec3r const mean_color() const {assert(is_dirty_ == false);
+					return mean_color_;}
+	vec3r const mean_normal() const {assert(is_dirty == false);
+					 return mean_normal_;}
+	real  const mean_radius() const {assert(is_dirty_ == false);
+	                                 return mean_radius_;}
+
+	real const pos_sd() const {assert(is_dirty_ == false);
+			           return pos_sd_;}
+	real const color_sd() const {assert(is_dirty_ == false);
+		                     return color_sd_;}
+	real const normal_sd() const {assert(is_dirty_ == false);
+	                              return normal_sd_;}
+	real const radius_sd() const {assert(is_dirty_ == false);
+	                              return radius_sd_;}
+
+	histogram_t const color_histogram() const {assert(is_dirty_ == false);						       return color_histogram_;}
 
 	void set_dirty(bool dirty) {is_dirty_ = dirty;}
 	bool is_dirty() const {return is_dirty_;}
+
 private:
 
 	bool is_dirty_;
-	vec3r mean_;
-	real  sd_;
+	vec3r mean_pos_;
+	real  pos_sd_;
+
+	vec3r mean_color_;
+	real color_sd_;
+
+	vec3r mean_normal_;
+	real  normal_sd_;
+
+	real  mean_radius_;
+	real  radius_sd_;
+
+	histogram_t color_histogram_;
+
 };
 
 }
