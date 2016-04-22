799200f3a7dd5fd590c6681b2c1fa808605861ad
xyzrgbcarl <xyzrgbcarl@gmail.com>
2016-04-22 22:11:25 +0200

    Add copyright notice

--- lamure_core/include/lamure/math/matrix4.h
+++ lamure_core/include/lamure/math/matrix4.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2014 Bauhaus-Universitaet Weimar
+// This Software is distributed under the Modified BSD License, see license.txt.
+//
+// Virtual Reality and Visualization Research Group 
+// Faculty of Media, Bauhaus-Universitaet Weimar
+// http://www.uni-weimar.de/medien/vr
 
 #ifndef LAMURE_MATH_MATRIX4_H_
 #define LAMURE_MATH_MATRIX4_H_
--- lamure_core/src/lamure/util/memory.cpp
+++ lamure_core/src/lamure/util/memory.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2014 Bauhaus-Universitaet Weimar
+// This Software is distributed under the Modified BSD License, see license.txt.
+//
+// Virtual Reality and Visualization Research Group 
+// Faculty of Media, Bauhaus-Universitaet Weimar
+// http://www.uni-weimar.de/medien/vr
 
 #include <lamure/util/memory.h>
 
