d58d04f45da88ecdc4c0fa106987878a1e500453
Schlewinow <schlewinow@gmx.de>
2016-12-15 10:47:23 +0100

    fixed a type overflow issue in pvs_utils by changing uint32_t to size_t

--- potentially_visible_set_common/src/pvs_utils.cpp
+++ potentially_visible_set_common/src/pvs_utils.cpp
@@ -18,8 +18,8 @@ void analyze_grid_visibility(const grid* input_grid, const unsigned int& num_ste
 
     // Iterate over view cells and collect visibility data.
     size_t num_cells = input_grid->get_cell_count();
-    lamure::node_t total_num_nodes = 0;
-    lamure::node_t total_visible_nodes = 0;
+    size_t total_num_nodes = 0;
+    size_t total_visible_nodes = 0;
 
     for(size_t cell_index = 0; cell_index < num_cells; ++cell_index)
     {
@@ -37,10 +37,10 @@ void analyze_grid_visibility(const grid* input_grid, const unsigned int& num_ste
             model_visible_nodes += visibility[model_index].size();
         }
 
-        float occlusion = (1.0f - (float)model_visible_nodes / (float)model_num_nodes) * 100.0f;
+        double occlusion = (1.0 - (double)model_visible_nodes / (double)model_num_nodes) * 100.0;
         
-        float interval_size = 100.0f / (float)num_steps;
-        int occlusion_index = std::round((occlusion - interval_size * 0.5f) / interval_size);
+        double interval_size = 100.0 / (double)num_steps;
+        int occlusion_index = std::round((occlusion - interval_size * 0.5) / interval_size);
         occlusion_index = std::max(0, std::min(occlusion_index, (int)(num_steps - 1)));
 
         occlusion_percent_interval_counter[occlusion_index] += 1;
@@ -51,14 +51,14 @@ void analyze_grid_visibility(const grid* input_grid, const unsigned int& num_ste
         total_visible_nodes += model_visible_nodes;
     }
 
-    float occlusion = (1.0f - (float)total_visible_nodes / (float)total_num_nodes) * 100.0f;
+    double occlusion = (1.0 - (double)total_visible_nodes / (double)total_num_nodes) * 100.0;
 
     // Output resulting data.
     file_out << "\ntotal: " << " occlusion: " << occlusion << "   " << total_visible_nodes << "/" << total_num_nodes << std::endl << std::endl;
     
     for(size_t occlusion_index = 0; occlusion_index < occlusion_percent_interval_counter.size(); ++occlusion_index)
     {
-        float occlusion_local = ((float)occlusion_percent_interval_counter[occlusion_index] / (float)num_cells) * 100.0f;
+        double occlusion_local = ((double)occlusion_percent_interval_counter[occlusion_index] / (double)num_cells) * 100.0;
         file_out << "occlusion interval " << occlusion_index << ": " << occlusion_local << "   (" << occlusion_percent_interval_counter[occlusion_index] << "/" << num_cells << ")" << std::endl;
     }
 
