36884cbcd931223dbc4e4b991135427452772fae
xyzrgbcarl <xyzrgbcarl@gmail.com>
2017-12-10 14:31:27 +0100

    Quality settings

--- apps/vis/main.cpp
+++ apps/vis/main.cpp
@@ -49,13 +49,9 @@
 #include <vector>
 #include <algorithm>
 
-uint32_t update_ms_ = 16;
 bool rendering_ = false;
-int32_t window_width_ = 1280;
-int32_t window_height_ = 720;
-float frame_div = 1;
-uint64_t frame_ = 0;
-bool fast_travel_ = false;
+int32_t render_width_ = 1280;
+int32_t render_height_ = 720;
 
 float near_plane_ = 0.001f;
 float far_plane_ = 1000.f;
@@ -70,8 +66,6 @@ std::vector<std::string> input_files_;
 std::vector<scm::math::mat4d> model_transformations_;
 int32_t num_models_ = 0;
 int32_t selected_model_ = -1;
-
-float error_threshold_ = 2.0f;
 
 float height_divided_by_top_minus_bottom_ = 0.f;
 
@@ -125,6 +119,7 @@ lamure::ren::Data_Provenance data_provenance_;
 struct settings {
   int32_t width_;
   int32_t height_;
+  int32_t frame_div_;
   int32_t vram_;
   int32_t ram_;
   int32_t upload_;
@@ -132,12 +127,15 @@ struct settings {
   int32_t splatting_;
   int32_t gamma_correction_;
   int32_t info_;
+  int32_t travel_;
+  float travel_speed_;
   int32_t pvs_cull_;
   int32_t show_normals_;
   int32_t show_accuracy_;
   int32_t show_output_sensitivity_;
   int32_t channel_;
-  float point_size_;
+  float point_scale_;
+  float error_threshold_;
   int32_t heatmap_;
   float heatmap_min_;
   float heatmap_max_;
@@ -232,6 +230,9 @@ void load_settings(std::string const& vis_file_name, settings& settings) {
           else if (key == "height") {
             settings.height_ = std::max(atoi(value.c_str()), 64);
           }
+          else if (key == "frame_div") {
+            settings.frame_div_ = std::max(atoi(value.c_str()), 1);
+          }
           else if (key == "vram") {
             settings.vram_ = std::max(atoi(value.c_str()), 8);
           }
@@ -249,6 +250,9 @@ void load_settings(std::string const& vis_file_name, settings& settings) {
           }
           else if (key == "info") {
             settings.info_ = std::max(atoi(value.c_str()), 0);
+          }
+          else if (key == "speed") {
+            settings.travel_speed_ = std::min(std::max(atof(value.c_str()), 0.0), 400.0);
           }
           else if (key == "pvs_cull") {
             settings.pvs_cull_ = std::max(atoi(value.c_str()), 0);
@@ -268,8 +272,11 @@ void load_settings(std::string const& vis_file_name, settings& settings) {
           else if (key == "channel") {
             settings.channel_ = std::max(atoi(value.c_str()), 0);
           }
-          else if (key == "point_size") {
-            settings.point_size_ = std::min(std::max(atof(value.c_str()), 0.0), 10.0);
+          else if (key == "point_scale") {
+            settings.point_scale_ = std::min(std::max(atof(value.c_str()), 0.0), 10.0);
+          }
+          else if (key == "error") {
+            settings.error_threshold_ = std::min(std::max(atof(value.c_str()), 0.0), 10.0);
           }
           else if (key == "background_color_r") {
             settings.background_color_.x = std::min(std::max(atoi(value.c_str()), 0), 255)/255.f;
@@ -359,11 +366,6 @@ bool cmd_option_exists(char** begin, char** end, const std::string& option) {
   return std::find(begin, end, option) != end;
 }
 
-static void glut_timer(int e) {
-  glutPostRedisplay();
-  glutTimerFunc(update_ms_, glut_timer, 1);
-}
-
 void draw_all_models(const lamure::context_t context_id, const lamure::view_t view_id, scm::gl::program_ptr shader) {
 
   lamure::ren::controller* controller = lamure::ren::controller::get_instance();
@@ -406,7 +408,7 @@ void draw_all_models(const lamure::context_t context_id, const lamure::view_t vi
     shader->uniform("model_view_matrix", scm::math::mat4f(model_view_matrix));
     shader->uniform("inv_mv_matrix", scm::math::mat4f(scm::math::transpose(scm::math::inverse(model_view_matrix))));
 
-    const scm::math::mat4f viewport_scale = scm::math::make_scale(window_width_ * 0.5f, window_width_ * 0.5f, 0.5f);;
+    const scm::math::mat4f viewport_scale = scm::math::make_scale(render_width_ * 0.5f, render_width_ * 0.5f, 0.5f);;
     const scm::math::mat4f viewport_translate = scm::math::make_translation(1.0f,1.0f,1.0f);
     const scm::math::mat4d model_to_screen =  scm::math::mat4d(viewport_scale) * scm::math::mat4d(viewport_translate) * model_view_projection_matrix;
     shader->uniform("model_to_screen_matrix", scm::math::mat4f(model_to_screen));
@@ -457,12 +459,12 @@ void draw_all_models(const lamure::context_t context_id, const lamure::view_t vi
 void set_uniforms(scm::gl::program_ptr shader) {
 
 
-    shader->uniform("win_size", scm::math::vec2f(window_width_, window_height_));
+    shader->uniform("win_size", scm::math::vec2f(render_width_, render_height_));
 
     shader->uniform("height_divided_by_top_minus_bottom", height_divided_by_top_minus_bottom_);
     shader->uniform("near_plane", near_plane_);
     shader->uniform("far_minus_near_plane", far_plane_-near_plane_);
-    shader->uniform("point_size_factor", settings_.point_size_);
+    shader->uniform("point_size_factor", settings_.point_scale_);
 
     shader->uniform("ellipsify", true);
     shader->uniform("clamped_normal_mode", true);
@@ -496,10 +498,15 @@ void glut_display() {
   
   if (settings_.info_) {
     std::ostringstream text_ss;
-    text_ss << "fps: " << std::setprecision(2) << fps_ << "\n";
+    text_ss << "fps: " << (int32_t)fps_ << "\n";
     text_ss << "# points: " << std::setprecision(2) << (rendered_splats_ / 1000000.0) << " mio.\n";
     text_ss << "# nodes: " << std::to_string(rendered_nodes_) << "\n";
-    text_ss << "pvs: " << pvs->is_activated() << "\n";
+    text_ss << "\n";
+    text_ss << "splatting (q): " << settings_.splatting_ << "\n";
+    text_ss << "point_scale (u/j): " << std::setprecision(2) << settings_.point_scale_ << "\n";
+    text_ss << "error (i/k): " << std::setprecision(2) << settings_.error_threshold_ << "\n";
+    text_ss << "pvs (p): " << pvs->is_activated() << "\n";
+    text_ss << "speed (f/F): " << std::setprecision(3) << settings_.travel_speed_ << "\n";
     text_ = text_ss.str();
   }
 
@@ -517,7 +524,7 @@ void glut_display() {
     lamure::model_t m_id = controller->deduce_model_id(std::to_string(model_id));
 
     cuts->send_transform(context_id, m_id, scm::math::mat4f(model_transformations_[m_id]));
-    cuts->send_threshold(context_id, m_id, error_threshold_);
+    cuts->send_threshold(context_id, m_id, settings_.error_threshold_);
     cuts->send_rendered(context_id, m_id);
     
     database->get_model(m_id)->set_transform(scm::math::mat4f(model_transformations_[m_id]));
@@ -562,7 +569,7 @@ void glut_display() {
     context_->set_depth_stencil_state(depth_state_less_);
     
     vis_xyz_pass1_shader_->uniform("near_plane", near_plane_);
-    vis_xyz_pass1_shader_->uniform("point_size_factor", settings_.point_size_);
+    vis_xyz_pass1_shader_->uniform("point_size_factor", settings_.point_scale_);
     vis_xyz_pass1_shader_->uniform("height_divided_by_top_minus_bottom", height_divided_by_top_minus_bottom_);
     vis_xyz_pass1_shader_->uniform("far_minus_near_plane", far_plane_-near_plane_);
 
@@ -570,7 +577,7 @@ void glut_display() {
     vis_xyz_pass1_shader_->uniform("clamped_normal_mode", true);
     vis_xyz_pass1_shader_->uniform("max_deform_ratio", 0.35f);
 
-    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(window_width_, window_height_)));
+    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(render_width_, render_height_)));
     context_->apply();
 
     draw_all_models(context_id, view_id, vis_xyz_pass1_shader_);
@@ -593,7 +600,7 @@ void glut_display() {
 
     set_uniforms(vis_xyz_pass2_shader_);
 
-    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(window_width_, window_height_)));
+    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(render_width_, render_height_)));
     context_->apply();
 
     draw_all_models(context_id, view_id, vis_xyz_pass2_shader_);
@@ -613,11 +620,11 @@ void glut_display() {
     vis_xyz_pass3_shader_->uniform_sampler("in_color_texture", 0);
     context_->bind_texture(pass2_color_buffer_, filter_nearest_, 0);
 
-    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(window_width_, window_height_)));
+    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(render_width_, render_height_)));
     context_->apply();
 
     screen_quad_->draw(context_);
-
+    
   }
   else {
     //single pass
@@ -634,7 +641,7 @@ void glut_display() {
     
     set_uniforms(vis_xyz_shader_);
 
-    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(window_width_, window_height_)));
+    context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(render_width_, render_height_)));
     context_->apply();
 
     draw_all_models(context_id, view_id, vis_xyz_shader_);
@@ -653,14 +660,14 @@ void glut_display() {
   context_->bind_texture(fbo_color_buffer_, filter_linear_, 0);
   quad_shader_->uniform("gamma_correction", (bool)settings_.gamma_correction_);
 
-  context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(window_width_, window_height_)));
+  context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(settings_.width_, settings_.height_)));
   context_->apply();
   
   screen_quad_->draw(context_);
 
   if (settings_.info_) {
     renderable_text_->text_string(text_);
-    text_renderer_->draw_shadowed(context_, scm::math::vec2i(20, window_height_- 40), renderable_text_);
+    text_renderer_->draw_shadowed(context_, scm::math::vec2i(20, settings_.height_- 40), renderable_text_);
   }
 
   rendering_ = false;
@@ -679,34 +686,42 @@ void glut_display() {
 void create_framebuffers() {
 
   fbo_ = device_->create_frame_buffer();
-  fbo_color_buffer_ = device_->create_texture_2d(scm::math::vec2ui(window_width_, window_height_), scm::gl::FORMAT_RGBA_32F , 1, 1, 1);
-  fbo_depth_buffer_ = device_->create_texture_2d(scm::math::vec2ui(window_width_, window_height_), scm::gl::FORMAT_D24, 1, 1, 1);
+  fbo_color_buffer_ = device_->create_texture_2d(scm::math::vec2ui(render_width_, render_height_), scm::gl::FORMAT_RGBA_32F , 1, 1, 1);
+  fbo_depth_buffer_ = device_->create_texture_2d(scm::math::vec2ui(render_width_, render_height_), scm::gl::FORMAT_D24, 1, 1, 1);
   fbo_->attach_color_buffer(0, fbo_color_buffer_);
   fbo_->attach_depth_stencil_buffer(fbo_depth_buffer_);
 
   pass1_fbo_ = device_->create_frame_buffer();
-  pass1_depth_buffer_ = device_->create_texture_2d(scm::math::vec2ui(window_width_, window_height_), scm::gl::FORMAT_D24, 1, 1, 1);
+  pass1_depth_buffer_ = device_->create_texture_2d(scm::math::vec2ui(render_width_, render_height_), scm::gl::FORMAT_D24, 1, 1, 1);
   pass1_fbo_->attach_depth_stencil_buffer(pass1_depth_buffer_);
 
   pass2_fbo_ = device_->create_frame_buffer();
-  pass2_color_buffer_ = device_->create_texture_2d(scm::math::vec2ui(window_width_, window_height_), scm::gl::FORMAT_RGBA_32F, 1, 1, 1);
+  pass2_color_buffer_ = device_->create_texture_2d(scm::math::vec2ui(render_width_, render_height_), scm::gl::FORMAT_RGBA_32F, 1, 1, 1);
   pass2_fbo_->attach_color_buffer(0, pass2_color_buffer_);
 
 }
 
 
 void glut_resize(int32_t w, int32_t h) {
-  window_width_ = w;
-  window_height_ = h;
+  settings_.width_ = w;
+  settings_.height_ = h;
+
+  render_width_ = settings_.width_ / settings_.frame_div_;
+  render_height_ = settings_.height_ / settings_.frame_div_;
+
   create_framebuffers();
   
   lamure::ren::policy* policy = lamure::ren::policy::get_instance();
-  policy->set_window_width(w);
-  policy->set_window_height(h);
+  policy->set_window_width(render_width_);
+  policy->set_window_height(render_height_);
   
-  context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(w, h)));
+  context_->set_viewport(scm::gl::viewport(scm::math::vec2ui(0, 0), scm::math::vec2ui(render_width_, render_height_)));
 
-  camera_->set_projection_matrix(30.0f, float(w)/float(h),  near_plane_, far_plane_);
+  camera_->set_projection_matrix(30.0f, float(settings_.width_)/float(settings_.height_),  near_plane_, far_plane_);
+
+  text_renderer_->projection_matrix(
+    scm::math::make_ortho_matrix(0.0f, static_cast<float>(settings_.width_),
+    0.0f, static_cast<float>(settings_.height_), -1.0f, 1.0f));
 
 }
 
@@ -721,7 +736,7 @@ void glut_keyboard(unsigned char key, int32_t x, int32_t y) {
       break;
 
     case 'q':
-      //settings_.splatting_ = !settings_.splatting_;
+      settings_.splatting_ = !settings_.splatting_;
       break;
 
     case 'p':
@@ -732,31 +747,58 @@ void glut_keyboard(unsigned char key, int32_t x, int32_t y) {
       }
 
     case 'u':
-      if (settings_.point_size_ > 0.1) {
-        settings_.point_size_ -= 0.1;
-        std::cout << "point_size: " << settings_.point_size_ << std::endl;
+      if (settings_.point_scale_ >= 0.2) {
+        settings_.point_scale_ -= 0.1;
       }
       break;
 
     case 'j':
-      if (settings_.point_size_ < 10.0) {
-        settings_.point_size_ += 0.1;
-        std::cout << "point_size: " << settings_.point_size_ << std::endl;
+      if (settings_.point_scale_ < 1.9) {
+        settings_.point_scale_ += 0.1;
+      }
+      break;
+
+
+    case 'i':
+      settings_.error_threshold_ -= 0.1f;
+      if (settings_.error_threshold_ < LAMURE_MIN_THRESHOLD)
+        settings_.error_threshold_ = LAMURE_MIN_THRESHOLD;
+      break;
+
+    case 'k':
+      settings_.error_threshold_ += 0.1f;
+      if (settings_.error_threshold_ > LAMURE_MAX_THRESHOLD)
+        settings_.error_threshold_ = LAMURE_MAX_THRESHOLD;
+      break;
+    
+
+    case 'f':
+      {
+        ++settings_.travel_;
+        if(settings_.travel_ > 4){
+          settings_.travel_ = 0;
+        }
+        settings_.travel_speed_ = (settings_.travel_ == 0 ? 0.5f
+                  : settings_.travel_ == 1 ? 5.5f
+                  : settings_.travel_ == 2 ? 20.5f
+                  : settings_.travel_ == 3 ? 100.5f
+                  : 300.5f);
+        camera_->set_dolly_sens_(settings_.travel_speed_);
       }
       break;
       
-    
-    case 'f':
-      std::cout<<"fast travel: ";
-      if (fast_travel_) {
-        camera_->set_dolly_sens_(0.5f);
-        std::cout<<"OFF\n\n";
+      case 'F':
+      {
+        if(settings_.travel_ > 0){
+          --settings_.travel_;
+        }
+        settings_.travel_speed_ = (settings_.travel_ == 0 ? 0.5f
+                  : settings_.travel_ == 1 ? 5.5f
+                  : settings_.travel_ == 2 ? 20.5f
+                  : settings_.travel_ == 3 ? 100.5f
+                  : 300.5f);
+        camera_->set_dolly_sens_(settings_.travel_speed_);
       }
-      else {
-        camera_->set_dolly_sens_(20.5f);
-        std::cout<<"ON\n\n";
-      }
-      fast_travel_ = ! fast_travel_;
       break;
 
     case '0':
@@ -801,7 +843,7 @@ void glut_keyboard(unsigned char key, int32_t x, int32_t y) {
 
 void glut_motion(int32_t x, int32_t y) {
 
-  camera_->update_trackball(x,y, window_width_, window_height_, mouse_state_);
+  camera_->update_trackball(x,y, settings_.width_, settings_.height_, mouse_state_);
 
 }
 
@@ -822,10 +864,15 @@ void glut_mouse(int32_t button, int32_t state, int32_t x, int32_t y) {
     } break;
   }
 
-  trackball_x_ = 2.f * float(x - (window_width_/2))/float(window_width_) ;
-  trackball_y_ = 2.f * float(window_height_ - y - (window_height_/2))/float(window_height_);
+  trackball_x_ = 2.f * float(x - (settings_.width_/2))/float(settings_.width_) ;
+  trackball_y_ = 2.f * float(settings_.height_ - y - (settings_.height_/2))/float(settings_.height_);
   
   camera_->update_trackball_mouse_pos(trackball_x_, trackball_y_);
+}
+
+
+void glut_idle() {
+  glutPostRedisplay();
 }
 
 //checks for prefix AND removes it (+ whitespace) if it is found; 
@@ -893,7 +940,7 @@ int32_t main(int argc, char* argv[]) {
 
   putenv((char *)"__GL_SYNC_TO_VBLANK=0");
 
-  settings_ = settings{1920, 1080, 2048, 4096, 32, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1.f, 0, 0.f, 0.05f, 
+  settings_ = settings{1920, 1080, 1, 2048, 4096, 32, 0, 1, 1, 1, 2, 20.5f, 0, 0, 0, 0, 0, 1.f, LAMURE_DEFAULT_THRESHOLD, 0, 0.f, 0.05f, 
     scm::math::vec3f(LAMURE_DEFAULT_COLOR_R, LAMURE_DEFAULT_COLOR_G, LAMURE_DEFAULT_COLOR_B),
     scm::math::vec3f(68.f/255.f, 0.f, 84.f/255.f), scm::math::vec3f(251.f/255.f, 231.f/255.f, 35.f/255.f),
     "", "", std::vector<std::string>(), std::vector<scm::math::mat4d>()};
@@ -903,10 +950,10 @@ int32_t main(int argc, char* argv[]) {
   policy->set_max_upload_budget_in_mb(settings_.upload_);
   policy->set_render_budget_in_mb(settings_.vram_);
   policy->set_out_of_core_budget_in_mb(settings_.ram_);
-  window_width_ = settings_.width_;
-  window_height_ = settings_.height_;
-  policy->set_window_width(window_width_);
-  policy->set_window_height(window_height_);
+  render_width_ = settings_.width_ / settings_.frame_div_;
+  render_height_ = settings_.height_ / settings_.frame_div_;
+  policy->set_window_width(settings_.width_);
+  policy->set_window_height(settings_.height_);
   policy->set_size_of_provenance(settings_.prov_);
 
   if (policy->size_of_provenance() > 0) {
@@ -937,7 +984,7 @@ int32_t main(int argc, char* argv[]) {
   glutInitContextVersion(4, 4);
   glutInitContextProfile(GLUT_CORE_PROFILE);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
-  glutInitWindowSize(window_width_, window_height_);
+  glutInitWindowSize(settings_.width_, settings_.height_);
   glutInitWindowPosition(64, 64);
   glutCreateWindow(argv[0]);
   glutSetWindowTitle("lamure_vis");
@@ -947,6 +994,7 @@ int32_t main(int argc, char* argv[]) {
   glutKeyboardFunc(glut_keyboard);
   glutMotionFunc(glut_motion);
   glutMouseFunc(glut_mouse);
+  glutIdleFunc(glut_idle);
 
   device_.reset(new scm::gl::render_device());
 
@@ -1035,8 +1083,6 @@ int32_t main(int argc, char* argv[]) {
 
   glutShowWindow();
   
-  glutTimerFunc(update_ms_, glut_timer, 1);
-
   create_framebuffers();
 
   color_blending_state_ = device_->create_blend_state(true, scm::gl::FUNC_ONE, scm::gl::FUNC_ONE, scm::gl::FUNC_ONE, 
@@ -1080,8 +1126,8 @@ int32_t main(int argc, char* argv[]) {
     text_renderer_ = scm::make_shared<scm::gl::text_renderer>(device_);
     renderable_text_ = scm::make_shared<scm::gl::text>(device_, output_font, scm::gl::font_face::style_regular, "sick, sad world...");
     text_renderer_->projection_matrix(
-      scm::math::make_ortho_matrix(0.0f, static_cast<float>(window_width_),
-      0.0f, static_cast<float>(window_height_), -1.0f, 1.0f));
+      scm::math::make_ortho_matrix(0.0f, static_cast<float>(settings_.width_),
+      0.0f, static_cast<float>(settings_.height_), -1.0f, 1.0f));
     renderable_text_->text_color(scm::math::vec4f(1.0f, 1.0f, 0.0f, 1.0f));
     renderable_text_->text_kerning(true);
   }
