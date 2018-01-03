3dd53fcaaa026709db6d3d7fb319863cf18653e2
xyzrgbcarl <xyzrgbcarl@gmail.com>
2018-01-03 14:25:54 +0100

    Add copyright

--- provenance/include/lamure/prov/sparse_point.h
+++ provenance/include/lamure/prov/sparse_point.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2014 Bauhaus-Universitaet Weimar
+// This Software is distributed under the Modified BSD License, see license.txt.
+//
+// Virtual Reality and Visualization Research Group 
+// Faculty of Media, Bauhaus-Universitaet Weimar
+// http://www.uni-weimar.de/medien/vr
+
 #ifndef LAMURE_SPARSE_POINT_H
 #define LAMURE_SPARSE_POINT_H
 
