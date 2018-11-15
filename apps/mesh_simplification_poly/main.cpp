0225a292c0212dc21f0f597be18408b0e632307b
gary444 <gareth.rendle@uni-weimar.de>
2018-11-15 15:47:45 +0100

    added utils class

--- apps/mesh_simplification_poly/main.cpp
+++ apps/mesh_simplification_poly/main.cpp
@@ -32,16 +32,19 @@
 #include <scm/core.h>
 #include <scm/core/math.h>
 
+#include "Utils.h"
+#include "OBJ_printer.h"
+
 // // //define a new vertex type - inheriting from base type CGAL::HalfedgeDS_vertex_base
 //ref https://doc.cgal.org/4.7/Polyhedron/index.html#title11
-template <class Refs, class T, class P, class Traits>
+template <class Refs, class T, class P>
 struct XtndVertex : public CGAL::HalfedgeDS_vertex_base<Refs, T, P>  {
     
   using CGAL::HalfedgeDS_vertex_base<Refs, T, P>::HalfedgeDS_vertex_base;
 
 };
 
-// namespace Traits{
+//extended point class
 template <class Traits>
 struct XtndPoint : public Traits::Point_3 {
 
@@ -55,174 +58,37 @@ struct XtndPoint : public Traits::Point_3 {
   : Traits::Point_3(x, y, z),
     texCoord(u, v) {}
 
-  
-
-  //XtndPoint(Traits const& a, Traits const& b, Traits const& c) 
-  //  : Traits::Point_3(a, b, c) {}
-  // typedef typename Traits::Point_3 Point;
   typedef typename Traits::Vector_2 TexCoord;
   TexCoord texCoord;
 
   double get_u () const {return texCoord.hx();}
   double get_v () const {return texCoord.hy();}
 
-  // using Traits::Point_3
 
 };
-// }
 
 // A new items type using extended vertex
+//TODO change XtndVertex back to original vertex class??
 struct Custom_items : public CGAL::Polyhedron_items_3 {
     template <class Refs, class Traits>
     struct Vertex_wrapper {
 
-
-      // typedef typename Traits::XtndPoint XtndPoint;
-      // typedef typename Traits::Vector_3 Normal;
-
-      typedef XtndVertex<Refs,CGAL::Tag_true, XtndPoint<Traits>,  Traits> Vertex;
+      typedef XtndVertex<Refs,CGAL::Tag_true, XtndPoint<Traits>> Vertex;
     };
 };
 
 
-
-// //from http://cgal-discuss.949826.n4.nabble.com/Extended-polyhedra-amp-nef-polyhedra-td4656726.html
-// // struct Custom_items : public CGAL::Polyhedron_items_3 { 
-
-// //   // wrap vertex
-// //   template <class Refs, class Traits>
-// //   struct Vertex_wrapper
-// //   {
-// //       typedef typename Traits::Point_3  Point;
-// //       typedef typename Traits::Vector_3 Normal;
-// //       typedef XtndVertex<Refs,CGAL::Tag_true,Point,Normal> Vertex;
-// //   }; 
-// };
-
-
 typedef CGAL::Simple_cartesian<double> Kernel;
 typedef CGAL::Polyhedron_3<Kernel, Custom_items> Polyhedron;
-// typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
 typedef Polyhedron::HalfedgeDS HalfedgeDS;
 
 
 namespace SMS = CGAL::Surface_mesh_simplification ;
 
 
-// struct GVertex
-// {
-//   Kernel::Point_3 v_pos;
-//   Kernel::Point_2 t_coord;
-//   Kernel::Point_3 normal;
-// };
 
-char* getCmdOption(char ** begin, char ** end, const std::string & option) {
-  char ** itr = std::find(begin, end, option);
-  if (itr != end && ++itr != end) {
-      return *itr;
-  }
-  return 0;
-}
-
-bool cmdOptionExists(char** begin, char** end, const std::string& option) {
-  return std::find(begin, end, option) != end;
-}
-
-//parses a face string like "f  2//1  8//1  4//1 " into 3 given arrays
-void parse_face_string (std::string face_string, int (&index)[3], int (&coord)[3], int (&normal)[3]){
-
-  //split by space into faces
-  std::vector<std::string> faces;
-  boost::algorithm::trim(face_string);
-  boost::algorithm::split(faces, face_string, boost::algorithm::is_any_of(" "), boost::algorithm::token_compress_on);
-
-  for (int i = 0; i < 3; ++i)
-  {
-    //split by / for indices
-    std::vector<std::string> inds;
-    boost::algorithm::split(inds, faces[i], [](char c){return c == '/';}, boost::algorithm::token_compress_off);
-
-    for (int j = 0; j < (int)inds.size(); ++j)
-    {
-      int idx = 0;
-      //parse value from string
-      if (inds[j] != ""){
-        idx = stoi(inds[j]);
-      }
-      if (j == 0){index[i] = idx;}
-      else if (j == 1){coord[i] = idx;}
-      else if (j == 2){normal[i] = idx;}
-      
-    }
-  }
-}
-
-// load obj function from vt_obj_loader/Utils.h
-void load_obj(const std::string& filename, 
-              std::vector<double> &v, 
-              std::vector<int> &vindices, 
-              std::vector<double> &t,
-              std::vector<int> &tindices ){
-
-  // std::vector<double> v;
-  // std::vector<int> vindices;
-  // std::vector<double> n;
-  // std::vector<int> nindices;
-  // std::vector<double> t;
-  // std::vector<int> tindices;
-
-  FILE *file = fopen(filename.c_str(), "r");
-
-  if (0 != file) {
-
-    while (true) {
-      char line[128];
-      int32_t l = fscanf(file, "%s", line);
-
-      if (l == EOF) break;
-      if (strcmp(line, "v") == 0) {
-        double vx, vy, vz;
-        fscanf(file, "%lf %lf %lf\n", &vx, &vy, &vz);
-        v.insert(v.end(), {vx, vy, vz});
-      } 
-      // else if (strcmp(line, "vn") == 0) {
-      //   float nx, ny, nz;
-      //   fscanf(file, "%f %f %f\n", &nx, &ny, &nz);
-      //   n.insert(n.end(), {nx,ny, nz});
-      // } 
-      else if (strcmp(line, "vt") == 0) {
-        float tx, ty;
-        fscanf(file, "%f %f\n", &tx, &ty);
-        t.insert(t.end(), {tx, ty});
-      } 
-      else if (strcmp(line, "f") == 0) {
-        fgets(line, 128, file);
-        std::string face_string = line; 
-        int index[3];
-        int coord[3];
-        int normal[3];
-
-        parse_face_string(face_string, index, coord, normal);
-
-        //here all indices are decremented by 1 to fit 0 indexing schemes
-        vindices.insert(vindices.end(), {index[0]-1, index[1]-1, index[2]-1});
-        tindices.insert(tindices.end(), {coord[0]-1, coord[1]-1, coord[2]-1});
-        // nindices.insert(nindices.end(), {normal[0]-1, normal[1]-1, normal[2]-1});
-      }
-    }
-
-    fclose(file);
-
-    std::cout << "positions: " << v.size()/3 << std::endl;
-    // std::cout << "normals: " << n.size()/3 << std::endl;
-    std::cout << "coords: " << t.size()/2 << std::endl;
-    std::cout << "faces: " << vindices.size()/3 << std::endl;
-
-  }
-
-}
-
-// load obj function from vt_obj_loader/Utils.h
+//assigns a tex coord to each vertex by matching a coordinate and a tex coord that are
+//used for the same vertex in the OBJ file
 void build_t_coords(std::vector<double> &v, 
                     std::vector<int> &vindices, 
                     std::vector<double> &t,
@@ -256,7 +122,7 @@ void build_t_coords(std::vector<double> &v,
   }
 }
 
-// // A modifier creating a triangle with the incremental builder.
+// implementatio of a Polyhedron_incremental_builder_3 to create the  polyhedron
 //http://jamesgregson.blogspot.com/2012/05/example-code-for-building.html
 template<class HDS>
 class polyhedron_builder : public CGAL::Modifier_base<HDS> {
@@ -273,9 +139,6 @@ public:
                       : vertices(_vertices), tris(_tris), t_coords(_t_coords) {}
 
   void operator()( HDS& hds) {
-    // typedef typename HDS::Vertex   Vertex;
-    // typedef typename XtndPoint xPoint;
-    // typedef typename Vertex::Normal Normal;
  
     // create a cgal incremental builder
     CGAL::Polyhedron_incremental_builder_3<HDS> B( hds, true);
@@ -284,7 +147,19 @@ public:
     // add the polyhedron vertices
     for( int i=0; i<(int)vertices.size() / 3; ++i ){
 
-      B.add_vertex( XtndPoint<Kernel>( vertices[(i*3)], vertices[(i*3)+1], vertices[(i*3)+2] , t_coords[i*2],  t_coords[(i*2)+1]));
+      //dummy textture coordinates
+      B.add_vertex( XtndPoint<Kernel>( vertices[(i*3)], 
+                                       vertices[(i*3)+1], 
+                                       vertices[(i*3)+2] , 
+                                       i,  
+                                       i+1
+                                       ));
+
+      // B.add_vertex( XtndPoint<Kernel>( vertices[(i*3)], 
+      //                            vertices[(i*3)+1], 
+      //                            vertices[(i*3)+2] , 
+      //                            t_coords[i*2],  
+      //                            t_coords[(i*2)+1]));
     }
    
     // add the polyhedron triangles
@@ -329,16 +204,16 @@ void see_polyhedron(const Polyhedron& P){
 int main( int argc, char** argv ) 
 {
   std::string obj_filename = "dino.obj";
-  if (cmdOptionExists(argv, argv+argc, "-f")) {
-    obj_filename = std::string(getCmdOption(argv, argv + argc, "-f"));
+  if (Utils::cmdOptionExists(argv, argv+argc, "-f")) {
+    obj_filename = std::string(Utils::getCmdOption(argv, argv + argc, "-f"));
   }
   else {
     std::cout << "Please provide a obj filename using -f <filename.obj>" << std::endl;
     return 1;
   }
   std::string out_filename = "data/simplified_mesh.obj";
-  if (cmdOptionExists(argv, argv+argc, "-o")) {
-    out_filename = std::string(getCmdOption(argv, argv + argc, "-o"));
+  if (Utils::cmdOptionExists(argv, argv+argc, "-o")) {
+    out_filename = std::string(Utils::getCmdOption(argv, argv + argc, "-o"));
   }
 
 
@@ -347,7 +222,7 @@ int main( int argc, char** argv )
   std::vector<int> tris;
   std::vector<double> t_coords;
   std::vector<int> tindices;
-  load_obj( obj_filename, vertices, tris, t_coords, tindices);
+  Utils::load_obj( obj_filename, vertices, tris, t_coords, tindices);
 
   if (vertices.size() == 0 ) {
     std::cout << "didnt find any vertices" << std::endl;
@@ -405,7 +280,8 @@ int main( int argc, char** argv )
 
   //write to file
   std::ofstream ofs(out_filename);
-  CGAL::print_polyhedron_wavefront(ofs, polyMesh);
+  // CGAL::print_polyhedron_wavefront(ofs, polyMesh);
+  OBJ_printer::print_polyhedron_wavefront_with_tex(ofs, polyMesh);
   ofs.close();
   std::cout << "simplified mesh was written to " << out_filename << std::endl;
 
