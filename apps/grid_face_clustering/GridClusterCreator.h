8d90b86b450c7ee7c8206f2f8e58b09cdf0cc4d4
gary444 <gareth.rendle@uni-weimar.de>
2019-01-24 22:26:21 +0100

    implemented correction for split charts after grid clustering. need to test

--- apps/grid_face_clustering/GridClusterCreator.h
+++ apps/grid_face_clustering/GridClusterCreator.h
@@ -1,3 +1,4 @@
+#include <stack>
 
 typedef scm::math::vec<int, 3> vec3i;
 
@@ -77,15 +78,18 @@ struct GridClusterCreator
 		}
 
 // TODO complete
-		// correct_split_charts(P,chart_id_map, charts_created.size());
+		uint32_t charts_added = correct_split_charts(P,chart_id_map, charts_created.size());
+		std::cout << "correct_split_charts method added " << charts_added << " Charts" << std::endl;
 
-		std::cout << "Created " <<  charts_created.size() << " charts \n"; 
+		std::cout << "Created " <<  charts_created.size() + charts_added << " charts in total \n"; 
 
-		return charts_created.size();
+		return charts_created.size() + charts_added;
 
 	}
 
-	static void correct_split_charts(Polyhedron &P,
+	//seeks chart areas that are not connected, and assigns new ids to unconnected areas
+	//returns number of charts added
+	static uint32_t correct_split_charts(Polyhedron &P,
 									 std::map<uint32_t, uint32_t> &chart_id_map,
 									 uint32_t num_charts_created){
 
@@ -96,32 +100,113 @@ struct GridClusterCreator
 		//ideally all faces will be included in final set
 
 		uint32_t faces_processed = 0;//debug only
-		uint32_t additional_charts = 0;
-
+		uint32_t additional_charts_overall = 0;
+		//for each chart id
 		for (uint32_t chart_id = 0; chart_id < num_charts_created; ++chart_id)
 		{
 			//build group of faces in chart from chart id map
-			std::set<uint32_t> faces;
+			std::set<uint32_t> chart_faces;
 			for ( Facet_iterator fb = P.facets_begin(); fb != P.facets_end(); ++fb){
 				if (chart_id_map[fb->id()] == chart_id)
 				{
-					faces.insert(fb->id());
+					chart_faces.insert(fb->id());
 				}
 			}
-			faces_processed += faces.size();//debug only
+			faces_processed += chart_faces.size();//debug only
 
-			// std::stack<uint32_t> face_stack;
+			std::stack<Facet_handle> faces_to_process;
+			std::set<uint32_t> processed_faces;
+
+			uint32_t additional_charts_for_this_chart = 0;
 
 			//TODO complete
+
+			//pick first face in chart faces
+			//get neighbours 
+			//if nbr face is in chart_faces, but not in processed faces, add to faces to process stack
+
+			//if stack is empty, and not all chart faces have been processed, then create repeat process from a different starting point
+
+			while (processed_faces.size() < chart_faces.size()){
+
+				//get starting face
+				//must not be in processed faces, but be in faces
+				for(uint32_t f : chart_faces){
+					if (processed_faces.find(f) == processed_faces.end())//if not processed faces in set
+					{
+						//add this face as starting face
+						Facet_iterator f_it = P.facets_begin();
+						std::advance(f_it,f);
+
+						//debug
+						if (f_it->id() != (int)f) {
+							std::cout << "[correct_split_charts] Error: face id is incorrect" << std::endl;
+						}
+
+						faces_to_process.push(f_it);
+						break;
+					}
+				}
+
+				//error checking only - stack should not be empty here
+				if (faces_to_process.empty()){
+					std::cout << "[correct_split_charts] Error: not all faces have been processed, but appropriate starting face was not found. (Chart " << chart_id << ")\n";
+				}
+
+				while (!faces_to_process.empty()){
+
+					//get and remove top node
+					Facet_handle top_node = faces_to_process.top();
+					faces_to_process.pop();
+					//recird that this face has been processed
+					processed_faces.insert(top_node->id());
+
+					//get neighbours of top node
+					Halfedge_facet_circulator he = top_node->facet_begin();
+				    do {
+				      Facet_handle nbr_facet = he->opposite()->facet(); 
+				      uint32_t nbr_facet_id = nbr_facet->id();
+
+				      //if this neighbour appears in chart faces...
+				      if (chart_faces.find(nbr_facet_id) != chart_faces.end())
+				      {
+				      	//and if its not in processed faces...
+				      	if (processed_faces.find(nbr_facet_id) == processed_faces.end())
+				      	{
+				      		//then add to stack to process later
+				      		faces_to_process.push(nbr_facet);
+
+				      		//and adjust chart id accordingly
+				      		//if this is found on the first pass, no action needed
+				      		//otherwise new id needs to be calculated
+				      		if (additional_charts_for_this_chart > 0)
+				      		{
+				      			uint32_t new_id = num_charts_created + additional_charts_overall + additional_charts_for_this_chart;
+				      			chart_id_map[nbr_facet_id] = new_id;
+				      		}
+				      	}
+				      }
+
+				    } while ( ++he != top_node->facet_begin());
+
+
+				} // end faces_to_process while
+
+
+				additional_charts_for_this_chart++;
+
+			}//end chart while
 			
+			additional_charts_overall += additional_charts_for_this_chart;
 
-		}
-
+		}//end for chart
 
 		if (P.size_of_facets() != faces_processed)//debug only
 		{
 			std::cout << "WARNING: [correct_split_charts] processed " << faces_processed << " faces, should have processed " << P.size_of_facets() << std::endl;
 		}
+
+		return additional_charts_overall;
 	}
 
 	//convert 3D cell reference to ID number
