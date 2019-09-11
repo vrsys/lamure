513c8b7ee0da28c88b59296f4234c815a6245aa8
xyzrgbcarl <xyzrgbcarl@gmail.com>
2019-09-11 15:25:58 +0200

    Improve pcl splitter app

--- apps/pcl_splitter/main.cpp
+++ apps/pcl_splitter/main.cpp
@@ -32,19 +32,47 @@ static bool cmd_option_exists(char **begin, char **end, const std::string &optio
     return std::find(begin, end, option) != end;
 }
 
+void get_string(const lamure::pre::surfel& surfel, std::string& line, bool xyz_all) {
 
+  line += std::to_string(surfel.pos().x) + " ";
+  line += std::to_string(surfel.pos().y) + " ";
+  line += std::to_string(surfel.pos().z) + " ";
+
+  if (xyz_all) {
+    line += std::to_string(surfel.normal().x) + " ";
+    line += std::to_string(surfel.normal().y) + " ";
+    line += std::to_string(surfel.normal().z) + " ";
+  }
+
+  line += std::to_string((int)surfel.color().x) + " ";
+  line += std::to_string((int)surfel.color().y) + " ";
+  line += std::to_string((int)surfel.color().z) + " ";
+
+  if (xyz_all) {
+    line += std::to_string(surfel.radius()) + " ";
+  }
+
+  line += "\n";
+
+}
 
 int main(int argc, char *argv[]) {
 
   bool terminate = false;
-  std::string input_filename = "";
+  std::string input_xyz_filename = "";
+  std::string input_bbx_filename = "";
   
   double splitting_distance = 1.0;
 
   if (cmd_option_exists(argv, argv + argc, "-i")) {
-    input_filename = std::string(get_cmd_option(argv, argv + argc, "-i"));
+    input_xyz_filename = std::string(get_cmd_option(argv, argv + argc, "-i"));
   }
   else terminate = true;
+
+
+  if (cmd_option_exists(argv, argv + argc, "-b")) {
+    input_bbx_filename = std::string(get_cmd_option(argv, argv + argc, "-b"));
+  }
 
   if (cmd_option_exists(argv, argv + argc, "-f")) {
     splitting_distance = atof(get_cmd_option(argv, argv + argc, "-f"));
@@ -55,6 +83,7 @@ int main(int argc, char *argv[]) {
     std::cout << "Usage: " << argv[0] << "<flags>\n" <<
       "INFO: " << argv[0] << "\n" <<
       "\t-i: select input .txt file with all .xyz files line by line\n" <<
+      "\t-b: select input .txt file with all .bbx files line by line (OPTIONAL)\n" <<
       "\t-f: select splitting distance (effectively the edge length of one cell)\n" << 
       std::endl;
     std::exit(0);
@@ -62,9 +91,10 @@ int main(int argc, char *argv[]) {
   
   //load all xyz filenames
   std::vector<std::string> xyz_filenames;
+  std::vector<std::string> bbx_filenames;
 
   {
-    std::ifstream input_file(input_filename.c_str());
+    std::ifstream input_file(input_xyz_filename.c_str());
 
     std::string line;
     while (getline(input_file, line)) {
@@ -74,6 +104,29 @@ int main(int argc, char *argv[]) {
     }
 
     input_file.close();
+  }
+
+
+  
+  if (input_bbx_filename != "") {
+    std::ifstream input_file(input_bbx_filename.c_str());
+
+    std::string line;
+    while (getline(input_file, line)) {
+
+      line.erase(std::remove(line.begin(),line.end(),' '), line.end());
+      bbx_filenames.push_back(line);
+    }
+
+    input_file.close();
+  }
+  
+
+  if (bbx_filenames.size() > 0) {
+    if (bbx_filenames.size() != xyz_filenames.size()) {
+      std::cout << "WARNING! The number of bbx files does not match the number of xyz files. Terminating..." << std::endl;
+      exit(0);
+    }
   }
 
   //loop all input files to determine the global bounding box
@@ -87,44 +140,89 @@ int main(int argc, char *argv[]) {
 
   for (uint32_t i = 0; i < xyz_filenames.size(); i++) {
 
-    std::string xyz_filename = xyz_filenames[i];
-    std::cout << "Open file " << xyz_filename << " ..." << std::endl;
+    if (bbx_filenames.size() > i) {
 
-    xyz_all = (input_filename.substr(input_filename.size()-8) == ".xyz_all");
-    if (!xyz_all) {
-      if (input_filename.substr(input_filename.size()-4) != ".xyz") {
-        std::cout << "ERROR: Invalid input format. Expected .xyz or .xyz_all" << std::endl;
-        std::exit(1);
+      std::string bbx_filename = bbx_filenames[i];
+      std::cout << "Open bbx file " << bbx_filename << " ..." << std::endl;
+
+      std::ifstream bbx_file(bbx_filename.c_str());
+
+      std::cout << "Expaning bounding box..." << std::endl;
+
+      std::string line;
+      while(getline(bbx_file, line)) {
+
+        std::istringstream lineparser(line);
+
+        std::string dummy;
+        lineparser >> dummy;
+        std::cout << "dummy: " << dummy << std::endl;
+
+        scm::math::vec3d pos;
+        lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.x;
+        lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.y;
+        lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.z;
+
+        std::cout << "pos " << pos << std::endl;
+
+
+        box_min.x = std::min(box_min.x, pos.x);
+        box_min.y = std::min(box_min.y, pos.y);
+        box_min.z = std::min(box_min.z, pos.z);
+
+        box_max.x = std::max(box_max.x, pos.x);
+        box_max.y = std::max(box_max.y, pos.y);
+        box_max.z = std::max(box_max.z, pos.z);
+
       }
-    }
 
-    std::ifstream xyz_file(xyz_filename.c_str());
-
-    std::cout << "Expaning bounding box..." << std::endl;
-  
-    //iterate all points
-
-    std::string line;
-    while(getline(xyz_file, line)) {
-
-      std::istringstream lineparser(line);
-
-      scm::math::vec3d pos;
-      lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.x;
-      lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.y;
-      lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.z;
-
-      box_min.x = std::min(box_min.x, pos.x);
-      box_min.y = std::min(box_min.y, pos.y);
-      box_min.z = std::min(box_min.z, pos.z);
-
-      box_max.x = std::max(box_max.x, pos.x);
-      box_max.y = std::max(box_max.y, pos.y);
-      box_max.z = std::max(box_max.z, pos.z);
+      bbx_file.close();
 
     }
+    else {
 
-    xyz_file.close();
+
+      std::string xyz_filename = xyz_filenames[i];
+      std::cout << "Open xyz file " << xyz_filename << " ..." << std::endl;
+
+      xyz_all = (xyz_filename.substr(xyz_filename.size()-8) == ".xyz_all");
+      if (!xyz_all) {
+        if (xyz_filename.substr(xyz_filename.size()-4) != ".xyz") {
+          std::cout << "ERROR: Invalid input format. Expected .xyz or .xyz_all" << std::endl;
+          std::exit(1);
+        }
+      }
+
+      std::ifstream xyz_file(xyz_filename.c_str());
+
+      std::cout << "Expaning bounding box..." << std::endl;
+    
+      //iterate all points
+
+      std::string line;
+      while(getline(xyz_file, line)) {
+
+        std::istringstream lineparser(line);
+
+        scm::math::vec3d pos;
+        lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.x;
+        lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.y;
+        lineparser >> std::setprecision(DEFAULT_PRECISION) >> pos.z;
+
+        box_min.x = std::min(box_min.x, pos.x);
+        box_min.y = std::min(box_min.y, pos.y);
+        box_min.z = std::min(box_min.z, pos.z);
+
+        box_max.x = std::max(box_max.x, pos.x);
+        box_max.y = std::max(box_max.y, pos.y);
+        box_max.z = std::max(box_max.z, pos.z);
+
+      }
+
+      xyz_file.close();
+
+    }
+
 
     std::cout << "\tcurrent min: " << box_min << std::endl;
     std::cout << "\tcurrent max: " << box_max << std::endl;
@@ -149,17 +247,23 @@ int main(int argc, char *argv[]) {
   std::cout << "Creating " << num_cells << " output files ..." << std::endl;
 
   std::vector<std::string> output_filenames;
-  std::vector<std::ofstream> output_files;
+  std::vector<std::vector<lamure::pre::surfel>> output_surfels;
 
   for (uint32_t i = 0; i < num_cells; ++i) {
     if (xyz_all) {
-      output_filenames.push_back(input_filename.substr(0, input_filename.size()-4) + "_cell_" + std::to_string(i) + ".xyz_all");
+      output_filenames.push_back(input_xyz_filename.substr(0, input_xyz_filename.size()-4) + "_cell_" + std::to_string(i) + ".xyz_all");
     }
     else {
-      output_filenames.push_back(input_filename.substr(0, input_filename.size()-4) + "_cell_" + std::to_string(i) + ".xyz");
+      output_filenames.push_back(input_xyz_filename.substr(0, input_xyz_filename.size()-4) + "_cell_" + std::to_string(i) + ".xyz");
     }
 
-    output_files.push_back(std::ofstream(output_filenames.back().c_str(), std::ios::trunc));
+    //truncate
+#if 1
+    std::ofstream output_file(output_filenames.back().c_str(), std::ios::trunc);
+    output_file.close();
+#endif
+
+    output_surfels.push_back(std::vector<lamure::pre::surfel>());
   }
   
   std::cout << "Starting 2nd pass ..." << std::endl;
@@ -171,9 +275,9 @@ int main(int argc, char *argv[]) {
     std::string xyz_filename = xyz_filenames[i];
     std::cout << "Open file " << xyz_filename << " ..." << std::endl;
 
-    xyz_all = (input_filename.substr(input_filename.size()-8) == ".xyz_all");
+    xyz_all = (xyz_filename.substr(xyz_filename.size()-8) == ".xyz_all");
     if (!xyz_all) {
-      if (input_filename.substr(input_filename.size()-4) != ".xyz") {
+      if (xyz_filename.substr(xyz_filename.size()-4) != ".xyz") {
         std::cout << "ERROR: Invalid input format. Expected .xyz or .xyz_all" << std::endl;
         std::exit(1);
       }
@@ -222,45 +326,69 @@ int main(int argc, char *argv[]) {
       pos -= box_min;
 
       scm::math::vec3ui cell;
-      cell.x = (uint32_t)std::ceil(pos.x / splitting_distance);
-      cell.y = (uint32_t)std::ceil(pos.y / splitting_distance);
-      cell.z = (uint32_t)std::ceil(pos.z / splitting_distance);
+      cell.x = (uint32_t)(pos.x / splitting_distance);
+      cell.y = (uint32_t)(pos.y / splitting_distance);
+      cell.z = (uint32_t)(pos.z / splitting_distance);
 
       uint32_t file_idx = cell.x 
                         + cell.y * num_cells_per_axis.x
                         + cell.z * (num_cells_per_axis.x*num_cells_per_axis.y);
 
-      output_files[file_idx] << std::setprecision(DEFAULT_PRECISION) << surfel.pos().x << " ";
-      output_files[file_idx] << std::setprecision(DEFAULT_PRECISION) << surfel.pos().y << " ";
-      output_files[file_idx] << std::setprecision(DEFAULT_PRECISION) << surfel.pos().z << " ";
+      output_surfels[file_idx].push_back(surfel);
 
-      if (xyz_all) {
-        output_files[file_idx] << std::setprecision(DEFAULT_PRECISION) << surfel.normal().x << " ";
-        output_files[file_idx] << std::setprecision(DEFAULT_PRECISION) << surfel.normal().y << " ";
-        output_files[file_idx] << std::setprecision(DEFAULT_PRECISION) << surfel.normal().z << " ";
+      if (output_surfels[file_idx].size() > 1000000) {
+
+        std::cout << "flushing 1M points to " << output_filenames[file_idx] << "..." << std::endl;
+
+        std::string line = "";
+        for (const auto& surfel : output_surfels[file_idx]) {
+          get_string(surfel, line, xyz_all);
+        }
+
+
+        std::ofstream output_file(output_filenames[file_idx].c_str(), std::ios::app);
+        output_file << line;
+        output_file.close();
+        
+        output_surfels[file_idx].clear();
+
+        line = "";
+
+        //std::cout << line << std::endl;
       }
 
-      output_files[file_idx] << (int)surfel.color().x << " ";
-      output_files[file_idx] << (int)surfel.color().y << " ";
-      output_files[file_idx] << (int)surfel.color().z << " ";
 
-      if (xyz_all) {
-        output_files[file_idx] << std::setprecision(DEFAULT_PRECISION) << surfel.radius() << " ";
-      }
-
-      output_files[file_idx] << std::endl;
-
-
-    }    
+    }
 
     xyz_file.close();
  
 
   }
 
-  for (auto& of : output_files) {
-    of.close();
+  //flush remaining points
+
+  for (uint32_t file_idx = 0; file_idx < output_surfels.size(); ++file_idx) {
+    if (output_surfels[file_idx].size() > 0) {
+
+      std::cout << "flushing points to " << output_filenames[file_idx] << "..." << std::endl;
+
+      std::string line = "";
+      for (const auto& surfel : output_surfels[file_idx]) {
+        get_string(surfel, line, xyz_all);
+      }
+
+      std::ofstream output_file(output_filenames[file_idx].c_str(), std::ios::app);
+      output_file << line;
+      output_file.close();
+      
+      output_surfels[file_idx].clear();
+
+      line = "";
+
+
+    }
   }
+
 
   //done
 
